81525a25a849d6ea78e9e97b6e5f2293ee4c36f8
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-06 12:19:25 +0800

    添加 footer 和 header 和 section

--- src/pages/hello/index.page.tsx
+++ src/pages/hello/index.page.tsx
@@ -6,16 +6,13 @@ import Hello from './hello.mdx'
 function Page() {
   return (
     <>
-      <h1>HTML + JS</h1>
-      <p>This page is rendered to HTML and has only few lines of browser-side JavaScript.</p>
-      <p>
-        <button>Loading...</button>
-      </p>
-      <p>
-        HMR works for CSS: modify <code>pages/html-js/index.css</code> to change the color of this{' '}
-        <span className="colored">red text</span>.
-      </p>
-      <Hello />
+      <section className="section">
+        <h1 className="title">Section</h1>
+        <h2 className="subtitle">
+          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+        </h2>
+        <Hello />
+      </section>
     </>
   )
 }
--- src/renderer/PageLayout.tsx
+++ src/renderer/PageLayout.tsx
@@ -1,28 +1,91 @@
 export { PageLayout }
 
-import './PageLayout.css'
+import 'bulma/css/bulma.css'
 import React from 'react'
 import type { PropsWithChildren } from 'react'
 
 function PageLayout({ children }: PropsWithChildren) {
   return (
     <React.StrictMode>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+          </a>
+
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">
+              Home
+            </a>
+
+            <a className="navbar-item">
+              Documentation
+            </a>
+
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">
+                More
+              </a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item">
+                  About
+                </a>
+                <a className="navbar-item">
+                  Jobs
+                </a>
+                <a className="navbar-item">
+                  Contact
+                </a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
       <Layout>
         <Content>{children}</Content>
       </Layout>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>animated-pancake</strong> by <a href="https://culcavy.github.io/animated-pancake">hollykbuck</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+        </div>
+      </footer>
     </React.StrictMode>
   )
 }
 
 function Layout({ children }: PropsWithChildren) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        maxWidth: 900,
-        margin: 'auto'
-      }}
-    >
+    <div className='container'>
       {children}
     </div>
   )
@@ -30,14 +93,7 @@ function Layout({ children }: PropsWithChildren) {
 
 function Content({ children }: PropsWithChildren) {
   return (
-    <div
-      style={{
-        padding: 20,
-        paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh'
-      }}
-    >
+    <div>
       {children}
     </div>
   )
