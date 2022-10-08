c35e73a6a71ed81bc5bb0a9f7487d3d30fe89634
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2022-10-08 16:21:17 -0600

    创建新页面 ffmpeg/intro

---
+++ src/pages/zh/ffmpeg/intro.page.tsx
@@ -0,0 +1,27 @@
+export { Page }
+
+import Hello from './Hello.mdx'
+
+function Page() {
+  return (
+    <>
+      <section className="bulma-hero bulma-is-primary">
+        <div className="bulma-hero-body">
+          <p className="bulma-title">
+            ffmpeg 入门
+          </p>
+          <p className="bulma-subtitle">
+            一点微小的经验
+          </p>
+        </div>
+      </section>
+      <section className="bulma-section">
+        <h1 className="bulma-title">Section</h1>
+        <h2 className="bulma-subtitle">
+          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+        </h2>
+        <Hello />
+      </section>
+    </>
+  )
+}
